d2002abc91fe9a532641a5f343de652ff05d1ac1
Nico Baier <nico.baier@gmail.com>
2025-02-28 00:05:49 -0600

    refactor(types.ts): expand types

--- src/types.ts
+++ src/types.ts
@@ -6,20 +6,36 @@ export type Env = {
 
 export type HNItem = {
 	id: number;
+	by?: string;
+	dead?: boolean;
+	deleted?: boolean;
 	descendants?: number;
-	by: string;
 	kids?: number[];
-	score: number;
+	parent?: number;
+	parts?: number[];
+	poll?: number;
+	score?: number;
 	text?: string;
-	time: number;
-	title: string;
-	type: "job" | "story" | "comment" | "poll" | "pollopt";
+	time?: number;
+	title?: string;
+	type?: "job" | "story" | "comment" | "poll" | "pollopt";
 	url?: string;
 };
 
-export interface NotificationResponse {
+export type NotificationResponse = {
 	id: number;
 	newComments: number;
 	url: string;
 	notification: boolean;
-}
+	type?: "job" | "story" | "comment" | "poll" | "pollopt";
+	text?: string;
+	title?: string;
+	parent?: number;
+};
+
+export type FollowedItem = {
+	key: string;
+	id: number;
+	comments: number;
+	url: string;
+};
